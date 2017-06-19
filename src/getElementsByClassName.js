f43b8959d08f472cb7d4a4cb408c808c331f49c9
Jesus Esquivel <JAER@Jesuss-MacBook-Air.local>
2017-06-18 21:04:36 -0500

    Requirements done!

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,28 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function(className) {
   // your code here
+  var arrayOfNodes = [];
+
+  var lookThroughNodes = function(element){
+    var children = element.childNodes;
+    //What is the base Case?
+    if(element.classList !== undefined && element.classList.contains(className)){
+      arrayOfNodes.push(element);
+    }
+   
+    children.forEach(function(child){
+      lookThroughNodes(child);
+    });
+  };  
+
+  lookThroughNodes(document.body);
+
+  return arrayOfNodes;
 };
+
+
+//document.body, element.childNodes, and element.classList
+
+
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,60 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
-};
+        // your code goes here
+
+        var stringify = '';
+
+        if (obj === null) {
+            stringify += 'null';
+            return stringify;
+        } else if (obj === true) {
+            stringify += 'true';
+            return stringify;
+        } else if (obj === false) {
+            stringify += 'false';
+            return stringify;
+        } else if (typeof obj === 'number') {
+            stringify += "" + obj;
+            return stringify;
+        } else if (typeof obj === 'string') {
+            stringify += '"' + obj + '"';
+            return stringify;
+        } else if (Array.isArray(obj)) {
+            stringify += '[';
+            for (var i = 0; i < obj.length; i++) {
+                stringify = stringify + stringifyJSON(obj[i]);
+                if (i + 1 < obj.length) {
+                    stringify += ',';
+                }
+            }
+            stringify += ']';
+            return stringify;
+        } else if (typeof obj === 'object' && !(null)) {
+            stringify += "{";
+            var index = 0;
+            //JSON.stringify({ x: 5, l: 6 });            // '{"x":5}'
+            for (var key in obj) {
+                if (stringifyJSON(obj[key])) {
+                    stringify += '"' + key + '":' + stringifyJSON(obj[key]);
+                    if (index + 1 < Object.keys(obj).length) {
+                        stringify += ',';
+
+                    }
+
+                    index++;
+
+                }
+
+
+                
+            }
+            stringify += "}";
+            
+        }
+        return stringify;
+
+    };
+
+
+        
